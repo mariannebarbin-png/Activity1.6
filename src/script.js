d09fa80200c84e331aaef40b66a486ff386c460a
mariannebarbin-png <mariannebarbin@gmail.com>
2025-10-17 18:39:14 +0800

    color

--- src/script.js
+++ src/script.js
@@ -17,7 +17,7 @@ scene.add(camera)
 
 const geometry = new THREE.SphereGeometry(1, 32, 32)
 const material = new THREE.MeshBasicMaterial({ 
-    color: 0xff9999
+    color: 0xffc0cb
 })
 const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
